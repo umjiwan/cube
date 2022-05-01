d9adee0c929dfc21b71edbb0deff5dac8b33d5ef
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-05-01 22:58:31 +0900

    doubleFront 완성

--- cube.py
+++ cube.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class cube:
     def __init__(self, cube=None):
         if not cube:
@@ -9,18 +11,19 @@ class cube:
         cube = []
         for i in range(1, 7):
             cube.append(np.full((3, 3), i))
-
-        """ test
+        
+        #test
+        
         cube[0] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
         cube[1] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
         cube[2] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
         cube[3] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
         cube[4] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
-        cube[5] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))"""
+        cube[5] = np.array(([1, 2, 3], [4, 5, 6], [7, 8, 9]))
 
         self.cube = cube
 
-    def showCube(self):
+    def showCube(self, color=True):
         floorPlan = np.zeros((9, 12))
         floorPlan[3:6, 3:6] = self.cube[0]
         floorPlan[3:6, 6:9] = self.cube[1]
@@ -29,7 +32,33 @@ class cube:
         floorPlan[0:3, 3:6] = self.cube[4]
         floorPlan[6:9, 3:6] = self.cube[5]
 
-        print(floorPlan)
+        floorPlan = floorPlan.tolist()
+
+        for i in floorPlan:
+            for v in i:
+                if color:
+                    if v == 0:
+                        colorCode = 30
+                    elif v == 1:
+                        colorCode = 37
+                    elif v == 2:
+                        colorCode = 34
+                    elif v == 3:
+                        colorCode = 33
+                    elif v == 4:
+                        colorCode = 32
+                    elif v == 5:
+                        colorCode = 91
+                    elif v == 6:
+                        colorCode = 31
+                    else:
+                        colorCode = 90
+
+                else:
+                    colorCode = 0
+
+                print(f"\033[{colorCode}m{int(v)}\033[0m. ", end="")
+            print("")
 
     def up(self, prime=False, count=1):
         for _ in range(count):
@@ -124,7 +153,6 @@ class cube:
                 cube[3][:, 2] = self.cube[4][2][::-1]
                 cube[4][2] = self.cube[1][:, 0]
 
-
             else:
                 cube[0] = np.rot90(self.cube[0], -1)
 
@@ -135,18 +163,36 @@ class cube:
 
             self.cube = np.array(cube, copy=True)
 
+    def doubleFront(self, prime=False, count=1):
+        for _ in range(count):
+            cube = np.array(self.cube, copy=True)
+            if prime:
+                cube[0] = np.rot90(self.cube[0])
+
+                cube[1][:, :2] = np.rot90(self.cube[5][:2])
+                cube[5][:2] = np.rot90(self.cube[3][:, 1:])
+                
+                cube[3][:, 1:] = np.rot90(self.cube[4][1:])
+                cube[4][1:] = np.rot90(self.cube[1][:, :2])
 
 
+            else:
+                cube[0] = np.rot90(self.cube[0], -1)
 
-import numpy as np
+                cube[1][:, :2] = np.rot90(self.cube[4][1:], -1)
+                cube[5][:2] = np.rot90(self.cube[1][:, :2], -1)
+                
+                cube[3][:, 1:] = np.rot90(self.cube[5][:2], -1)
+                cube[4][1:] = np.rot90(self.cube[3][:, 1:], -1)
 
-myCube = cube()
-myCube.newCube()
+            self.cube = np.array(cube, copy=True)
 
-myCube.doubleDown(count=3)
-myCube.front(prime=True, count=3)
-myCube.doubleDown(count=3)
-myCube.doubleUp(count=2)
-myCube.front(prime=False, count=3)
 
-myCube.showCube()
+if __name__ == "__main__":
+    myCube = cube()
+    myCube.newCube()
+
+    myCube.showCube()
+    print("")
+    myCube.doubleFront(prime=True)
+    myCube.showCube()
